f25b85b6cfd6df836566a52c38ca50153b3d0bf2
Kenneth Scribner <kennethscribner@yahoo.com>
2025-02-23 20:17:47 -0700

    New changes

--- main.py
+++ main.py
@@ -1,33 +1,3 @@
-MENU = {
-    'expresso': {
-        "ingredients": {
-            "water:": 50,
-            "coffee": 18,
-        },
-        "cost": 1.5
-    },
-    "latte": {
-        "ingredients": {
-            "water": 200,
-            "milk": 150,
-            "coffee": 24,
-        },
-        "cost": 2.5,
-    },
-    "cappuccino": {
-        "ingredients": {
-            "water": 250,
-            "milk": 100,
-            "coffee": 24,
-        },
-        "cost": 3.0,
-    }
-}
-
-RESOURCES = {
-    "water": 300,
-    "milk": 200,
-    "coffee": 100,
-}
-
-choice = input("What would you like? (expresso/latte/cappuccino): ")
+from menu import Menu
+from coffee_maker import CoffeeMaker
+from money_machine import MoneyMachine
