48a6298cbe987f4becc0341ed2e0911287d8daf7
Kenneth Scribner <kennethscribner@yahoo.com>
2025-02-23 21:13:37 -0700

    final

--- main.py
+++ main.py
@@ -22,5 +22,6 @@ while is_on:
         money_machine.report()
     else:
         drink = menu.find_drink(choice)
-        if coffee_maker.is_resource_sufficient(drink):
+        if coffee_maker.is_resource_sufficient(drink) and money_machine.make_payment(drink.cost):
+                coffee_maker.make_coffee(drink)
 
